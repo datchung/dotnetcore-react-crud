e93dbc53bcb1269871400cee66c93784a62121ec
datchung <datxchung@gmail.com>
2020-08-17 22:42:34 -0400

    Update to .NET Core 3.1

--- src/Startup.cs
+++ src/Startup.cs
@@ -18,7 +18,7 @@ namespace okta_dotnetcore_react_example
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
+            services.AddMvc(a => { a.EnableEndpointRouting = false; });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
